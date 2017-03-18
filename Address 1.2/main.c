6723260cd1d0d7ffc53077fe0ab8917720c6a30e
Adrian McDaniel <adriandmcdaniel@gmail.com>
2017-03-17 20:32:30 -0400

    Initial Commit

---
+++ Address 1.2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Address 1.2
+//
+//  Created by Adrian McDaniel on 3/17/17.
+//  Copyright © 2017 Adrian McDaniel. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
