991de4af59a0cfe5d76a0105f6e90d13d87049fd
Adrian McDaniel <adriandmcdaniel@gmail.com>
2017-03-21 13:24:09 -0400

    Address 1.2

--- Address 1.2/main.c
+++ Address 1.2/main.c
@@ -9,7 +9,17 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int i = 17;
+
+    int *addressOfI = &i;
+    printf("i stores it's value at %p\n", addressOfI);
+    
+    //you can use a  * operator on the lefthand side to  assign a new value or data in the address location
+    *addressOfI = 89;
+    printf("Now i is %d\n", i);
+    
+    //The sizeof() function returns a value type of size_t.  %zu is the placeholder token for size_t.
+    printf("An int is %zu bytes\n", sizeof(int));
+    printf("A pointer is %zu bytes\n", sizeof(int *));
     return 0;
 }
